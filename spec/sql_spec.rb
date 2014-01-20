5dbe24fa4e17ffeb882c2e1fc55e2d63e61bf6ac
Emmanuel Bastien <os@ebastien.name>
2014-01-21 00:46:53 +0100

    First iteration on value expression parsing.

--- spec/sql_spec.rb
+++ spec/sql_spec.rb
@@ -4,16 +4,29 @@ require 'sql/select'
 
 describe Sql::SelectParser do
 
-  let(:p) { Sql::SelectParser.new }
+  def parse(q)
+    r = Sql::SelectParser.new.parse q
+    puts r.value if r
+    r
+  end
+
+  it "parses select without from clause" do
+    parse("select a").should_not be_nil
+  end
+
+  it "parses select with from clause" do
+    parse("select a from t").should_not be_nil
+  end
+
+  it "parses select with from and where clauses" do
+    parse("select a from t where a = 1").should_not be_nil
+  end
 
   it "works" do
-    q = """
+    parse("""
 with q as (
   select ax, bx, cx from my_table as t
 ) select ax from q
-"""
-    r = p.parse(q)
-    puts r.value if r
-    expect(r).not_to be_nil
+""").should_not be_nil
   end
 end
