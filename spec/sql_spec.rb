ad6a784131830da2bb5e4a0a62c3ee7ccca9f1b9
Emmanuel Bastien <os@ebastien.name>
2014-02-23 00:03:03 +0100

    First spec on join. Workaround for left-recursion to be found.

--- spec/sql_spec.rb
+++ spec/sql_spec.rb
@@ -6,20 +6,34 @@ describe Sql::SelectParser do
 
   def parse(q)
     r = Sql::SelectParser.new.parse q
-    puts r.value if r
+    puts r if r
     r
   end
 
   it "parses select without from clause" do
-    parse("select a").should_not be_nil
+    t = parse("select a")
+    t.should_not be_nil
+    t.tables.should be_empty
   end
 
   it "parses select with from clause" do
     parse("select a from t").should_not be_nil
   end
 
+  it "parses select with two tables" do
+    parse("select a from t1, t2").should_not be_nil
+  end
+
   it "parses select with from and where clauses" do
     parse("select a from t where b + 1 = a and a > 2").should_not be_nil
+  end
+
+  it "parses select with subquery" do
+    parse("select a from t, (select b,c from d.t)").should_not be_nil
+  end
+
+  it "parses select with join" do
+    parse("select a,b from t1 inner join t2 on (t1.a = t2.a)").should_not be_nil
   end
 
   it "works" do
